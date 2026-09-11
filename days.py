513103f444a2526916166254156c98a817341754
janhavichorage <janhavichorage@gmail.com>
2026-09-11 15:39:11 +0530

    add this

---
+++ days.py
@@ -0,0 +1,22 @@
+
+
+num=int(input("enter your number:"))
+
+if num==1:
+    print("monday")
+
+elif num==2:
+    print("tuesday")
+
+elif num==3:
+    print("tuesday")
+
+elif num==4:
+    print("wednesday")
+elif num==5:
+    print("thursday")
+
+elif num==6:
+    print("friday")
+elif num==7:
+    print("sayurday")
